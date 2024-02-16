76d3431b785e09a40f69c0b25c35539f579ebbc9
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-02-16 08:02:18 +0530

    16-02-2024 Day-3

---
+++ VariablesEx.java
@@ -0,0 +1,27 @@
+class VariablesEx
+{	
+int  marks=234;//global variable every where inside the class we can access
+	public static void main(String args[])
+	{
+		byte age=28;//local variables inside method only we can access
+		long contact=949494949;
+		float salary=23.45379230235203f;
+		double result=23.45379230235203;
+		boolean value=false;
+		char gender='M';
+		
+	System.out.println("welcome to java");
+	System.out.println(age+" "+contact+" "+salary+" "+result+" "+value+" "+gender);
+	}
+}
+	// JDK-->JRE--->JVM -->0's 1's-->binary-->byte
+	
+	//translator-->trnaslation
+	
+	//compiler-->compailation
+	
+	//source code-->byte code
+	
+	//.java --> .class     javac filename.java
+	
+	//java classname
